2ca3fd6673e6f039f0c7c49276d5c576a811f219
joelleli <joelle.licho@rocketmail.com>
2024-01-30 10:16:06 +0000

    MERN-Stack Infrastructure

---
+++ controllers/api/users.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken")
+const User = require("../../models/user")
+const bcrypt = require("bcrypt")
+
+module.exports = {
+    create,
+    login,
+    checkToken
+}
+
+async function login(req, res) {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+
+        if (!user) {
+            throw new Error()
+        }
+        const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
+
+        if (!isPasswordValid) {
+            throw new Error()
+        }
+        const token = createJWT(user)
+
+        res.json(token)
+
+    } catch {
+        res.status(400).json("Bad Credentials")
+    }
+
+}
+
+async function create(req, res) {
+    try {
+        const user = await User.create(req.body)
+
+        const token = createJWT(user)
+
+        res.json(token)
+
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+function checkToken(req, res) {
+    console.log("req.user", req.user)
+    res.json(req.exp)
+}
+
+// Helper functions
+function createJWT(user) {
+    return jwt.sign(
+        { user },
+        process.env.SECRET,
+        { expiresIn: "24h" }
+    )
+}
